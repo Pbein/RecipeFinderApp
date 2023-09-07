1a8905a8e2406f6c07ebcb1fac972c9c8575df52
philip bein <philipbein10697@gmail.com>
2023-09-07 04:50:58 -0400

    Document and comment on userModel and recipeModel schemas
    Added comments to both userModel and recipeModel to better describe the data structure and functionality of each schema. This will ensure clearer understanding and easier future enhancements of the database schema.

--- backend/models/recipeModel.js
+++ backend/models/recipeModel.js
@@ -1,28 +1,36 @@
+// Import required package
 const mongoose = require('mongoose');
 
+// Define ingredient sub-schema for a recipe
 const ingredientSchema = new mongoose.Schema({
+    // Name of the ingredient
     name: {
         type: String,
         required: true,
         trim: true
     },
+    // Quantity or measurement for the ingredient
     quantity: {
         type: String,
         required: true,
         trim: true
     },
+    // Additional notes or description for the ingredient
     notes: {
         type: String,
         trim: true
     }
 });
 
+// Define instruction sub-schema for a recipe
 const instructionSchema = new mongoose.Schema({
+    // Name or title for the step
     step: {
         type: String,
         required: true,
         trim: true
     },
+    // Detailed description or action for the step
     details: {
         type: String,
         required: true,
@@ -30,7 +38,9 @@ const instructionSchema = new mongoose.Schema({
     }
 });
 
+// Define main recipe schema
 const recipeSchema = new mongoose.Schema({
+    // Title or name of the recipe
     title: {
         type: String,
         required: [true, 'Please enter a title'],
@@ -38,13 +48,17 @@ const recipeSchema = new mongoose.Schema({
         trim: true,
         maxlength: [100, 'Title cannot be more than 100 characters']
     },
+    // Brief summary or overview of the recipe
     description: {
         type: String,
         required: [true, 'Please enter a description'],
         maxlength: [500, 'Description cannot be more than 500 characters']
     },
+    // List of ingredients required for the recipe
     ingredients: [ingredientSchema],
+    // Step-by-step guide on preparing the recipe
     instructions: [instructionSchema]
 });
 
+// Export the Recipe model based on the recipeSchema
 module.exports = mongoose.model('Recipe', recipeSchema);
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,11 @@
+// Import required packages and modules
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+// Define user schema for MongoDB using mongoose
 const userSchema = new mongoose.Schema({
+    // Username field with validation rules
     username: {
         type: String,
         required: [true, 'Please enter a username'],
@@ -10,6 +13,7 @@ const userSchema = new mongoose.Schema({
         trim: true,
         maxlength: [20, 'Username cannot be more than 20 characters']
     },
+    // Email field with validation rules
     email: {
         type: String,
         required: [true, 'Please enter an email'],
@@ -17,10 +21,12 @@ const userSchema = new mongoose.Schema({
         trim: true,
         maxlength: [50, 'Email cannot be more than 50 characters']
     },
+    // Password field with validation rules
     password: {
         type: String,
         required: [true, 'Please enter a password'],
     },
+    // Flag to determine if the user is an admin
     isAdmin: {
         type: Boolean,
         required: true,
@@ -28,20 +34,24 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// Encrypt password using bcryptjs
+// Middleware to encrypt password before saving user to the database
 userSchema.pre('save', async function(next) {
-    const salt = await bcrypt.genSalt(10);  // Generate a salt with 10 rounds
-    this.password = await bcrypt.hash(this.password, salt);  // Hash the password with the salt
+    // Generate a salt for hashing
+    const salt = await bcrypt.genSalt(10);
+    // Hash and replace plain text password with the hashed version
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
-// Sign JWT and return
+// Method to generate a JWT for user authentication
 userSchema.methods.getSignedJwtToken = function() {
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
 }
 
-// Match user entered password to hashed password in DB
+// Method to compare provided password with the hashed password in the database
 userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 }
 
+// Export the User model based on the userSchema
 module.exports = mongoose.model('User', userSchema);
