7bb9c4a7765f8cc7ed74425a0f4aab203e8a0b60
philip bein <philipbein10697@gmail.com>
2023-10-15 20:42:10 -0400

    Created Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
